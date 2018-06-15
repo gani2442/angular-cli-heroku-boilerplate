7c1ab7abf392ac3ae593935ce98ff081519bba96
gani2442 <ganesh-p@hcl.com>
2018-06-15 21:37:40 +0530

    add car list chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,52 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { CarService } from './shared/car/car.service';
+import { GiphyService } from './shared/giphy/giphy.service';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CarListComponent } from './car-list/car-list.component';
+import {MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule} from '@angular/material';
+import { CarEditComponent } from './car-edit/car-edit.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes : Routes = [
+  {
+    path: '', redirectTo: '/car-list', pathMatch: 'full'
+  },
+  {
+    path: 'car-list',
+    component: CarListComponent
+  },
+  {
+    path: 'car-add',
+    component: CarEditComponent
+  },
+  {
+    path: 'car-edit/:id',
+    component: CarEditComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarListComponent
+    CarListComponent,
+    CarEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
-  providers: [CarService],
+  providers: [CarService, GiphyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/car/car.service.ts
+++ src/app/shared/car/car.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  constructor(private http: HttpClient) {
+  public API = 'http://localhost:8080';
+  public CAR_API = this.API + '/cars';
+  public COOL_CARS_API = this.API + '/cool-cars';
+
+  constructor(private http:HttpClient) { }
+
+  getAll() : Observable<any> {
+    return this.http.get(this.COOL_CARS_API);
   }
 
-  getAll(): Observable<any> {
-    return this.http.get(' https://kp-sbhb.herokuapp.com/cool-cars');
+  get(id : string) {
+    return this.http.get(this.CAR_API + "/" + id)
+  }
+
+  save(car : any): Observable<any> {
+    let result: Observable<Object>;
+    if (car['href']) {
+      result = this.http.put(car.href, car);
+    } else {
+      result = this.http.post(this.CAR_API, car);
+    }
+    return result;
+  }
+
+  remove(href: string) {
+    return this.http.delete(href);
   }
 }
